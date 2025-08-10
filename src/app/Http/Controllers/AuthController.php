e7ff20bba6cd8f78d02a7cf89aaad2cae520f20e
root <root@fs010001.>
2025-08-10 11:57:38 +0900

    feat: 出退勤と休憩機能

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -22,6 +22,7 @@ class AuthController extends Controller
             'name' => $request['name'],
             'email' => $request['email'],
             'password' => Hash::make($request['password']),
+            'status_id' => '1',
         ]);
         Auth::login($user);
         $user->sendEmailVerificationNotification();
